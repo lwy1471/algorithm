b82dceea66a57db335ffc8d0806ca0ed86c3ff13
이동훈 <honeybees@sk.com>
2019-12-18 19:17:38 +0900

    brute-force

--- code.plus/14500. Tetromino/Sol14500.java
+++ code.plus/14500. Tetromino/Sol14500.java
@@ -95,7 +95,7 @@ class Sol14500 {
                     max = sum > max ? sum : max;
                 }
                 //
-
+                //
                 if(arr[i][j+1] !=-1 && arr[i+1][j+1] !=-1 && arr[i+1][j+2] !=-1) {
                     int sum = arr[i][j]+arr[i][j+1]+arr[i+1][j+1]+arr[i+1][j+2];
                     max = sum > max ? sum : max;
