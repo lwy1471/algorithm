3713511b06c39c149e6617e1963c49db6fc60341
이동훈 <honeybees@naver.com>
2020-01-02 18:26:28 +0900

    solve permutation problem

--- code.plus/10974. All permutation/Solution10974.java
+++ code.plus/10974. All permutation/Solution10974.java
@@ -1,38 +1,40 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-class Sol10973 {
-    
+class Solution10974 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] array = new int[size];
-        for(int i=0;i<size;i++) {
-            array[i]=sc.nextInt();
-        }
+        StringBuilder sb = new StringBuilder();
 
-        if(!nextPermutation(array))
-            System.out.println("-1");
-        else {
-            for(int i=0;i<size;i++) {
-                if(i+1==size)
-                    System.out.println(array[i]);
-                else
-                    System.out.print(array[i]+" ");
-            }
+        int N = sc.nextInt();
+
+        int[] array = new int[N];
+        for (int i=0;i<N;i++) {
+            array[i]=i+1;
         }
+        Arrays.sort(array);
+        do {
+            for(int i=0;i<N;i++) {
+                sb.append(array[i]+" ");
+            }
+            sb.append("\n");
+        }while(nextPermutation(array));
+
+        System.out.println(sb);
+        sc.close();
     }
 
     static boolean nextPermutation(int[] array) {
         int size = array.length;
         int i=size-1;
-        while (i>0 && array[i-1] <= array[i]) i=i-1;
+        while (i>0 && array[i-1] >= array[i]) i=i-1;
         
         if(i<=0)
             return false;
 
         int j=size-1;
-        while (array[i-1] <= array[j]) j=j-1;
+        while (array[i-1] >= array[j]) j=j-1;
         swap(array, i-1, j);
 
         j=size-1;
